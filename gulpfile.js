e9216997d42d2f788521dab8bb456b90c324281d
Brian Mikinski <Brian Mikinski>
2016-11-23 16:09:31 -0600

    Creating task for gulp typescript compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,50 @@
-
 /*
  * Main entry point for gulp.js
  */
+'use strict'
 
+/**
+ * Load required npm packages
+ */
+var gulp = require("gulp"),
+            ts = require("gulp-typescript"),
+            sourceMaps = require("gulp-sourcemaps"),
+            del = require("del");
+
+/**
+ *  Configure paths
+ * */
+var paths = {
+    scripts: {
+        compile:{
+            input: "src/**/*.ts",
+            output:"wwwroot/",
+            config: "tsconfig.json"
+        }
+    }
+};
+
+var tsProject = ts.createProject(paths.scripts.compile.config,
+                {
+                    typescript: require("typescript")
+                });
+
+/**
+ * Clean 
+ */
+gulp.task("cleanTypeScript", function(){
+    return del([ paths.scripts.compile.output + "Scripts/**/*"])
+});
+
+/**
+ * Compile TypeScript
+ */
+gulp.task('compileTypeScript', ['cleanTypeScript'], function(){
+
+    return gulp.src(paths.scripts.compile.input)
+            .pipe(sourceMaps.init())
+            .pipe(tsProject())
+            .js
+            .pipe(sourceMaps.write('.'))
+            .pipe(gulp.dest(paths.scripts.compile.output))
+});
